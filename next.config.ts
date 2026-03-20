dd32ca8c3781ed24b2124d388466295930e6fffd
0xares <0xares0@gmail.com>
2026-03-20 10:16:48 +0100

    fix: Update withSerwist import

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 // next.config.js
-const { withSerwist } = require('@serwist/next').default;
+const withSerwist = require('@serwist/next').default;
 
 const withPWA = withSerwist({
   // Source service worker file you author

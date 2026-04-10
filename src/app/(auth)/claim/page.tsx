e1244639a49c11d14c9d55dc131b28a383d044ff
Ares <0xares0@gmail.com>
2026-04-10 17:28:59 +0100

    fix: Update pages

--- src/app/(auth)/claim/page.tsx
+++ src/app/(auth)/claim/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Suspense } from "react";
 import ClaimContent from "@/components/layout/claim-content";
 
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { DashboardRedirect } from "@/components/layout/auth-guard";
 
 /**

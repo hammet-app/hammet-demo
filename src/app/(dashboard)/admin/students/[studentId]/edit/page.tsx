13d2243ca00a5b37d03861dae1025deff37ad701
Ares <0xares0@gmail.com>
2026-05-11 18:37:23 +0200

    fix: ensure undefined values for empty student update fields in handleSubmit

--- src/app/(dashboard)/admin/students/[studentId]/edit/page.tsx
+++ src/app/(dashboard)/admin/students/[studentId]/edit/page.tsx
@@ -32,11 +32,11 @@ export default function UpdateStudentPage() {
     try {
         await updateStudent(studentId, 
             {
-              email: email.trim(), 
-              class_level:classLevel.trim(), 
-              class_arm: classArm.trim(), 
-              parent_email: parentEmail.trim(), 
-              parent_phone: parentPhone.trim()
+              email: email.trim() || undefined,
+              parent_phone: parentPhone.trim() || undefined,
+              class_level: classLevel.trim() || undefined,
+              class_arm: classArm.trim() || undefined,
+              parent_email: parentEmail.trim() || undefined,
             } satisfies UserUpdateRequest,
             accessToken,
             refreshToken
